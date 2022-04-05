d247b08a1de56e152275ab2823ad6d0d5be06c12
benson <benson@localhost.localdomain>
2022-04-06 00:29:54 +0800

    OHLC

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import stockplotly.stock_info as spsi
 
 def main():
+    #stock parameter
     ticker = 'msft'
-    startdate = '01/12/2019'
+    startdate = '01/12/2021'
     enddate = '01/04/2022'
     stock = spsi.Stock(ticker, startdate, enddate)
+
     # stock.history_price()
-    stock.candlestick()
+    # stock.candlestick()
+    stock.Ohlc()
 
 if __name__ == "__main__":
     main()
--- stockplotly/stock_info.py
+++ stockplotly/stock_info.py
@@ -13,18 +13,19 @@ class Stock():
 
         self.__history_price = si.get_data(
             ticker, 
-            start_date=start_date, 
-            end_date=end_date, 
+            start_date=self.__start_date, 
+            end_date=self.__end_date, 
             index_as_date=False,
         )
 
     def history_price(self):
         df = self.__history_price
+        suffix = ' History Price'
         df["ma10"] = df["adjclose"].rolling(10).mean()
         df["ma20"] = df["adjclose"].rolling(20).mean()
         df["ma60"] = df["adjclose"].rolling(60).mean()
 
-        fig = px.line(df, x="date", y='adjclose', title=self.__ticker.upper())
+        fig = px.line(df, x="date", y='adjclose', title=self.__ticker.upper()+suffix)
         fig.add_trace(go.Scatter(x=df.date, y=df.ma10,name="Ma10"))
         fig.add_trace(go.Scatter(x=df.date, y=df.ma20,name="Ma20"))
         fig.add_trace(go.Scatter(x=df.date, y=df.ma60,name="Ma60"))
@@ -33,6 +34,7 @@ class Stock():
 
     def candlestick(self):
         df = self.__history_price
+        suffix = ' Candlestick'
         fig = go.Figure(
             data = [go.Candlestick(
                 x = df.date,
@@ -43,9 +45,23 @@ class Stock():
                 increasing_line_color = 'red',
                 decreasing_line_color = 'green'
             )],
-            layout = go.Layout(
-                title=go.layout.Title(text=self.__ticker.upper())
-    )
+            layout = go.Layout(title=go.layout.Title(text=self.__ticker.upper()+suffix))
+        )
+
+        fig.show()
+
+    def Ohlc(self):
+        df = self.__history_price
+        suffix = ' Ohlc'
+        fig = go.Figure(
+            data = [go.Ohlc(
+                x = df.date,
+                open = df.open,
+                high = df.high,
+                low = df.low,
+                close = df.close,
+            )],
+            layout = go.Layout(title=go.layout.Title(text=self.__ticker.upper()+suffix))
         )
 
         fig.show()
